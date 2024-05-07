1e0d3b66ed8a5f728f873404e8e0e7fb2871bc86
pttlinh <pttlinh@gmail.com>
2024-05-07 21:45:34 +0700

    SplashSrceen

---
+++ src/constants/info.ts
@@ -0,0 +1,8 @@
+import {Dimensions} from 'react-native';
+
+export const info = {
+  sizes: {
+    WIDTH: Dimensions.get('window').width,
+    HEIGHT: Dimensions.get('window').height,
+  },
+};
--- src/screens/SplashScreen.tsx
+++ src/screens/SplashScreen.tsx
@@ -1,12 +1,40 @@
-import {View, Text} from 'react-native';
 import React from 'react';
+import {
+  ActivityIndicator,
+  Image,
+  ImageBackground,
+  StyleSheet,
+} from 'react-native';
+import {info} from '../constants/info';
+import {SpaceComponent} from '../components';
+import {colors} from '../constants/theme';
 
 const SplashScreen = () => {
   return (
-    <View>
-      <Text>SplashScreen</Text>
-    </View>
+    <ImageBackground
+      source={require('../assets/images/splash.png')}
+      style={styles.container}
+      imageStyle={{
+        flex: 1,
+      }}>
+      <Image
+        source={require('../assets/images/logo.png')}
+        style={{
+          width: info.sizes.WIDTH * 0.7,
+          resizeMode: 'contain',
+        }}
+      />
+      <SpaceComponent height={16} />
+      <ActivityIndicator color={colors.gray} size={22} />
+    </ImageBackground>
   );
 };
 
 export default SplashScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});

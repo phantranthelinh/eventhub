439335def48ac5b8425fc781a36d4bad72c7f1ef
pttlinh <pttlinh@gmail.com>
2024-05-07 21:50:07 +0700

    StatusBar

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import AuthNavigator from './src/navigations/AuthNavigator';
 import {SplashScreen} from './src/screens';
 import {NavigationContainer} from '@react-navigation/native';
+import {StatusBar} from 'react-native';
 const TIME = 2000;
 const App = () => {
   const [isShowSplashScreen, setIsShowSplashScreen] = useState(true);
@@ -16,13 +17,21 @@ const App = () => {
     };
   }, []);
 
-  console.log(isShowSplashScreen);
-  return isShowSplashScreen ? (
-    <SplashScreen />
-  ) : (
-    <NavigationContainer>
-      <AuthNavigator />
-    </NavigationContainer>
+  return (
+    <>
+      <StatusBar
+        barStyle="dark-content"
+        translucent
+        backgroundColor="transparent"
+      />
+      {!isShowSplashScreen ? (
+        <SplashScreen />
+      ) : (
+        <NavigationContainer>
+          <AuthNavigator />
+        </NavigationContainer>
+      )}
+    </>
   );
 };
 
